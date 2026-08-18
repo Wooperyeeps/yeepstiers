ec61763d47fb63fa64f4b66e8117111f8ecd700a
Voffi <Voffi910@outlook.com>
2026-08-18 12:36:27 +0200

    Create Players.js
    doing rest later

---
+++ Players.js
@@ -0,0 +1,20 @@
+export const players = [
+  { 
+    id:1, 
+    name:"", 
+    avatar:"", 
+    title:"", 
+    score:, 
+    rank:"",
+    categories:[
+      {cat:"Bat PvP", pos:"", img:""},
+      {cat:"Dagger PvP", pos:"", img:"https://yeeplist.page.gd/images/Bat_Tier4.png"},
+      {cat:".FG.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:".M.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Shield + Weapon PvP", pos:"Retired Tier: 3", img:"https://yeeplist.page.gd/images/Tier3.png"},
+      {cat:".F.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:".S.", pos:"", img:"https://yeeplist.page.gd/images/TierU.png"},
+      {cat:"Other PvP", pos:"Retired Tier: 4", img:"https://yeeplist.page.gd/images/Tier4.png"}
+    ]
+  },
+];
